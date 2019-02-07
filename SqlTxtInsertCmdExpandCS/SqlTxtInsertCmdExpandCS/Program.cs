a310c4f8e1230816b47a301dd22e0f62824242c2
Krzysztof Z <krisdeveloper25@gmail.com>
2019-02-07 12:38:16 -0600

    Added Visual Studio project and source code.

---
+++ SqlTxtInsertCmdExpandCS/SqlTxtInsertCmdExpandCS/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+using System.IO;
+
+namespace SqlTxtInsertCmdExpandCS
+{
+    class Program
+    {
+
+        static void WriteLineToFile(ref string sOutputFile, ref string sLine)
+        {
+            TextWriter tw = new StreamWriter(sOutputFile, true);
+            tw.Write(sLine);
+            tw.Close();
+        }
+
+        static void ExpandInsertIntoNewFile(ref string sqlFileAsText, ref string sOutputFile, ref int iTotalInserts)
+        {
+            string[] sqlCmdTextArray;
+            uint uiIndex = 0;
+            bool bIsParseLine = false;
+            string sNewLine = "";
+
+            sqlCmdTextArray = sqlFileAsText.Split(';');
+            foreach (string s in sqlCmdTextArray)
+            {
+                bIsParseLine = false;
+
+                if (s.Contains("CREATE TABLE"))
+                {
+                    TableRecords.AddTableFromDefintion(s);
+                }
+                else if (s.Contains("INSERT INTO"))
+                {
+                    sNewLine = TableRecords.ParseInsertStatement(s);
+                    bIsParseLine = true;
+                    iTotalInserts++;
+                }
+                else
+                {
+                    //Console.WriteLine("COMMAND: UNKNOWN/Not of Interest");
+                }
+
+                uiIndex++;
+
+                if (false == bIsParseLine)
+                {
+                    sNewLine = s + ";";
+                }
+
+                WriteLineToFile(ref sOutputFile, ref sNewLine);
+            }
+        }
+
+        static void ReadSqlFileAsText(ref string filePathNameIn, ref string fileAsTextOut)
+        {
+            string textLine = "";
+
+            if (true == System.IO.File.Exists(filePathNameIn))
+            {
+                System.IO.StreamReader objReader;
+                objReader = new System.IO.StreamReader(filePathNameIn);
+
+                do
+                {
+                    textLine = objReader.ReadLine() + "\r\n";
+                    fileAsTextOut += textLine;
+                } while (objReader.Peek() != -1);
+
+                objReader.Close();
+            }
+            else
+            {
+                Console.WriteLine("Cound not open file: " + filePathNameIn);
+            }
+        }
+
+        static string CreateOutFileName(string sFileInput)
+        {
+            string sFileOutput = "";
+            string sFileExt = "";
+            string sTimeStamp = DateTime.Now.ToString("yyyy-MM-dd_HHmmss");
+
+            sFileExt = Path.GetExtension(sFileInput);
+            if (".db" == sFileExt.ToLower())
+            {
+                sFileOutput = sFileInput.Replace(sFileExt, "." + sTimeStamp + sFileExt);
+            }
+            else if (".sql" == sFileExt.ToLower())
+            {
+                sFileOutput = sFileInput.Replace(sFileExt, "." + sTimeStamp + sFileExt);
+            }
+            else
+            {
+                Console.WriteLine("sFileExt: " + sFileExt);
+                sFileOutput = sFileInput + "." + sTimeStamp;
+            }
+
+            return sFileOutput;
+        }
+
+        static void Main(string[] args)
+        {
+            string sqlFileAsText = "";
+            string sFileInput = "";
+            string sFileOutput = "";
+            int iTotalInserts = 0;
+
+            Console.WriteLine("SQL Text File (*.sql) 'INSERT' Statement Expander");
+            Console.WriteLine(" PURPOSE: Make DIFF's easier.");
+            Console.WriteLine(" ");
+
+            // A parameter must be passed with the file name, otherwise exit
+            if (args.Length > 0)
+            {
+                sFileInput = args[0];
+                sFileOutput = CreateOutFileName(sFileInput);
+
+                Console.WriteLine("Input File:  " + sFileInput);
+                Console.WriteLine("Output File: " + sFileOutput);
+                Console.WriteLine("Working....");
+
+                // Read the *.sql file into one big string
+                ReadSqlFileAsText(ref sFileInput, ref sqlFileAsText);
+
+                // Write to the console
+                ExpandInsertIntoNewFile(ref sqlFileAsText, ref sFileOutput, ref iTotalInserts);
+
+                Console.WriteLine("Finished with Parsing, expanded {0} INSERT statements.", iTotalInserts);
+            }
+        }
+    }
+}
---
+++ SqlTxtInsertCmdExpandCS/SqlTxtInsertCmdExpandCS/TableRecords.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SqlTxtInsertCmdExpandCS
+{
+    class TableRecords
+    {
+        static Dictionary<string, TableRecordEntry> _hash = new Dictionary<string, TableRecordEntry>();
+
+        public static void AddTableFromDefintion(string strSqlTableDefEntry)
+        {
+            string strTableData = "";
+            string strTableName = "";
+            string strTableDefPrefix = "";
+
+            string pattern = @"CREATE TABLE `([a-zA-Z].*)` ";
+            Match match = Regex.Match(strSqlTableDefEntry, pattern);
+            if (match.Success)
+            {
+                strTableName = match.Groups[1].Value;
+
+                strTableDefPrefix = "\r\nCREATE TABLE `" + strTableName + "` (\r\n  ";
+                strTableData = strSqlTableDefEntry.Replace(strTableDefPrefix, "");
+
+                TableRecordEntry curTblEntry = new TableRecordEntry();
+                strTableData = strTableData.Replace("\n  ", "");
+                curTblEntry.StrItemArray = strTableData.Split('\r');
+                curTblEntry.CalculateMaxStrLen();
+
+                _hash.Add(strTableName, curTblEntry);
+            }
+            else
+            {
+                pattern = @"CREATE TABLE ([a-zA-Z].*)\(";
+                match = Regex.Match(strSqlTableDefEntry, pattern);
+                if (match.Success)
+                {
+                    strTableName = match.Groups[1].Value;
+
+                    strTableDefPrefix = "\r\nCREATE TABLE " + strTableName + "(\r\n   ";
+                    strTableData = strSqlTableDefEntry.Replace(strTableDefPrefix, "");
+
+                    TableRecordEntry curTblEntry = new TableRecordEntry();
+                    strTableData = strTableData.Replace("\n   ", "");
+                    curTblEntry.StrItemArray = strTableData.Split('\r');
+                    curTblEntry.CalculateMaxStrLen();
+
+                    _hash.Add(strTableName, curTblEntry);
+                }
+            }
+        }
+
+        public static string ParseInsertStatement(string strSqlInsertEntry)
+        {
+            string strSqlInsertEntryNew = "";
+            string strTblInsertData = "";
+            string strTblInsertTableName = "";
+
+            string pattern = @"INSERT INTO ""([a-zA-Z].*)"" ";
+            Match match = Regex.Match(strSqlInsertEntry, pattern);
+            if (match.Success)
+            {
+                // SYNTAX 1 ---------------------------------------------------
+                strTblInsertTableName = match.Groups[1].Value;
+
+                string strFind = "\r\nINSERT INTO \"" + strTblInsertTableName + "\" VALUES(";
+                strTblInsertData = strSqlInsertEntry.Replace(strFind, "");
+
+                bool bContainsTbl = _hash.ContainsKey(strTblInsertTableName);
+
+                strSqlInsertEntryNew += strFind;
+                strSqlInsertEntryNew += "\n"; // "\r\n"
+
+                string[] strValues = strTblInsertData.Split(',');
+                int iCount = 0;
+                foreach (string s in strValues)
+                {
+                    string strNewEntry = "";
+                    string strDesc = "";
+                    if (true == bContainsTbl)
+                    {
+                        strDesc = _hash[strTblInsertTableName].StrItemArray[iCount];
+                    }
+
+                    if (0 != iCount)
+                    {
+                        strSqlInsertEntryNew += ",";
+                        strSqlInsertEntryNew += "\n";  // "\r\n"
+                    }
+
+                    strNewEntry = "/* " + iCount.ToString().PadLeft(3, ' ') + ": " + strDesc + " */ " + s;
+                    strSqlInsertEntryNew += strNewEntry;
+
+                    iCount++;
+                }
+
+                strSqlInsertEntryNew += ";";
+            }
+            else
+            {
+                // SYNTAX 2 ---------------------------------------------------
+                pattern = @"INSERT INTO ([a-zA-Z].*) VALUES";
+                match = Regex.Match(strSqlInsertEntry, pattern);
+                if (match.Success)
+                {
+                    strTblInsertTableName = match.Groups[1].Value;
+
+                    string strFind = "\r\nINSERT INTO " + strTblInsertTableName + " VALUES(";
+                    strTblInsertData = strSqlInsertEntry.Replace(strFind, "");
+
+                    bool bContainsTbl = _hash.ContainsKey(strTblInsertTableName);
+
+                    strSqlInsertEntryNew += strFind;
+                    strSqlInsertEntryNew += "\n"; // "\r\n"
+
+                    string[] strValues = strTblInsertData.Split(',');
+                    int iCount = 0;
+                    foreach (string s in strValues)
+                    {
+                        string strNewEntry = "";
+                        string strDesc = "";
+                        if (true == bContainsTbl)
+                        {
+                            strDesc = _hash[strTblInsertTableName].StrItemArray[iCount];
+                        }
+
+                        if (0 != iCount)
+                        {
+                            strSqlInsertEntryNew += ",";
+                            strSqlInsertEntryNew += "\n";  // "\r\n"
+                        }
+
+                        strNewEntry = "/* " + iCount.ToString().PadLeft(3, ' ') + ": " + strDesc + " */ " + s;
+                        strSqlInsertEntryNew += strNewEntry;
+
+                        iCount++;
+                    }
+
+                    strSqlInsertEntryNew += ";";
+                }
+            }
+
+            return strSqlInsertEntryNew;
+        }
+
+        public static void WriteHashMapToConsole()
+        {
+            foreach (KeyValuePair<string, TableRecordEntry> pair in _hash)
+            {
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Table Name: " + pair.Key);
+                TableRecordEntry currTblEntry = pair.Value;
+                int iCount = 0;
+                foreach (string s in currTblEntry.StrItemArray)
+                {
+                    Console.WriteLine(iCount.ToString().PadLeft(3, ' ') + ": " + s);
+                    iCount++;
+                }
+            }
+        }
+
+        public class TableRecordEntry
+        {
+            public int MaxColumnCharLen { get; set; }
+            public string[] StrItemArray { get; set; }
+
+            public void CalculateMaxStrLen()
+            {
+                MaxColumnCharLen = 0;
+                foreach (string s in StrItemArray)
+                {
+                    if (s.Length > MaxColumnCharLen)
+                    {
+                        MaxColumnCharLen = s.Length;
+                    }
+                }
+            }
+
+            public void PadStringArray()
+            {
+                for (int iIndex = 0; iIndex < StrItemArray.Count(); iIndex++)
+                {
+                    string strLocal = StrItemArray[iIndex];
+                    int iCurrStrSize = strLocal.Length;
+                    if (iCurrStrSize < MaxColumnCharLen)
+                    {
+                        Console.WriteLine(strLocal);
+
+                        int iPadLen = (MaxColumnCharLen - iCurrStrSize);
+
+                        strLocal = strLocal.PadRight(iPadLen, '.');
+                        StrItemArray[iIndex] = strLocal;
+                    }
+                }
+            }
+        };
+
+    }
+}
